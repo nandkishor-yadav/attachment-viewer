5469996d870d1c0be094b78eb95b271ee63f3987
Nandkishor.Y <Nandkishor.Yadav@wrendata.com>
2021-11-23 20:46:59 +0000

    kk

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 import DocViewer, { DocViewerRenderers } from 'react-doc-viewer';
-import FileViewer from 'react-file-viewer';
+// import FileViewer from 'react-file-viewer';
+import dynamic from 'next/dynamic';
+const FileViewer = dynamic(() => import('react-file-viewer'));
 
 // https://view.officeapps.live.com/op/view.aspx?src=https%3A%2F%2Fattachment-viewer.vercel.app%2FVsCode-Shortcut.docx&wdOrigin=BROWSELINK
 
-https: export default function Home() {
+export default function Home() {
   const docs = [
     {
       uri: '/Critical-Skills-Employment-Permits-Checklist.pdf',
@@ -26,13 +28,16 @@ https: export default function Home() {
   const type1 = 'xlsx';
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <FileViewer fileType={type} filePath={file} />
-      <FileViewer fileType={type1} filePath={file1} />
-      );
-      <DocViewer pluginRenderers={DocViewerRenderers} documents={docs} />
-      <DocViewer pluginRenderers={DocViewerRenderers} documents={docs1} />
-      <DocViewer pluginRenderers={DocViewerRenderers} documents={docs2} />
-    </div>
+    <>
+      <div>
+        <FileViewer fileType={type} filePath={file} />
+        <FileViewer fileType={type1} filePath={file1} />
+      </div>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <DocViewer pluginRenderers={DocViewerRenderers} documents={docs} />
+        <DocViewer pluginRenderers={DocViewerRenderers} documents={docs1} />
+        <DocViewer pluginRenderers={DocViewerRenderers} documents={docs2} />
+      </div>
+    </>
   );
 }
